6c347334411a03020f9167033c8b9d2babad1f92
jaredw4332 <101159434+jaredw4332@users.noreply.github.com>
2023-10-21 14:45:53 -0700

    Change div to grid

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
+//Initialize grid space as a table and add the onclick event listener
+
 const gameboardContainer = document.getElementById('gameboardContainer')
 
-for (let i = 0; i < 64; i++){
-    let gamePiece = document.createElement('div')
-    gamePiece.classList.add('gamePiece')
-    gameboardContainer.appendChild(gamePiece)
+for (let i = 0; i < 15; i++){
+    let row=gameboardContainer.insertRow();
+    for(let j=0;j<15;j++){
+        let gamePiece = row.insertCell();
+        gamePiece.setAttribute("xPos",j);
+        gamePiece.setAttribute("yPos",i);
+        gamePiece.classList.add("gamePiece");
+        gamePiece.onclick= function(){
+            let coordinates={"x":-1,"y":-1}
+            coordinates.x=this.cellIndex;
+            coordinates.y=this.parentNode.rowIndex;
+            console.log(coordinates);
+        };
+    }
 }
